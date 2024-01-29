175232c6665ab2515e21cf76d588a1e8e7664057
Tim Janke <tim+github@janke.biz>
2024-01-29 09:15:04 +0100

    :bug: Fix an issue when not supplying a valid host list

--- src/scitsifreine/internal.py
+++ src/scitsifreine/internal.py
@@ -1,5 +1,6 @@
 def generate_session_name(host_list: [str], prefix='multissh'):
     session_name = f'{prefix}-'
-    for current_host in host_list:
-        session_name += f'{current_host.split(".")[0]}-'
+    if host_list:
+        for current_host in host_list:
+            session_name += f'{current_host.split(".")[0]}-'
     return session_name[:-1]
